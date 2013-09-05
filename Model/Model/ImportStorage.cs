cbc25cc8a3d494f760d338a6c4f8ffad8a9db86c
fjqi1214 <fjqi1214@sina.com>
2013-09-05 10:52:03 +0800

    create project

---
+++ Model/Model/ImportStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+
+    /// <summary>
+    /// 入库
+    /// </summary>
+    public class ImportStorage
+    {
+    }
+}
---
+++ Model/Model/manufacturer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    /// <summary>
+    /// 厂家
+    /// </summary>
+    public class manufacturer
+    {
+    }
+}
